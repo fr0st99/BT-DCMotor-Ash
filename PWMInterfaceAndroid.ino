d3c92bb0b9e8cb612a9d9ac9d093237c44c0cdf2
Ash <48594888+fr0st99@users.noreply.github.com>
2022-01-28 01:07:23 +0000

    Update PWMInterfaceAndroid.ino

--- PWMInterfaceAndroid.ino
+++ PWMInterfaceAndroid.ino
@@ -2,28 +2,53 @@
 
 #include<SoftwareSerial.h>
 SoftwareSerial bluetoothSerial(4,5);
+
+
 String msg,cmd;
 boolean motor_dir = 0;
 int speed_value = 0;
 int i = 0;
-
+int speed;
+float rev = 0;
+int rpm_val;
+int prev = 0;
+int time;
 
 
 #define pwm1     9   //input 2
 #define pwm2    10   //input 1
 
-int speed;
+
+
+void INTERRUPT()
+{
+  rev++;
+}
+
+
+
 
 void setup() {
   // Initialization
     Serial.begin(9600); // Communication rate of the Bluetooth Module
     msg = "";
+    attachInterrupt(1, INTERRUPT, RISING);
     bluetoothSerial.begin(9600);
     pinMode(pwm1,   OUTPUT);
     pinMode(pwm2,   OUTPUT);
 }
 
 void loop() {
+
+// RPM value calculation from IR proximity sensor data 
+  delay(1000);
+  detachInterrupt(0);                   
+  time = millis() - prev;          
+  rpm_val = (rev/time) * 60000;       
+  prev = millis();                  
+  rev = 0;
+  Serial.println(rpm_val);
+  attachInterrupt(1, INTERRUPT, RISING);
   
   // To read message received from other Bluetooth Device
   if (bluetoothSerial.available() > 0){ // Check if there is data coming
